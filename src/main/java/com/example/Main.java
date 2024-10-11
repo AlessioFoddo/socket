a956f060a8d1cc333384dcaed5540ac60b07d38b
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 10:31:46 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,51 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(3000);
+        System.out.println("Server avviato!");
+
+        Socket s = server.accept();
+        System.out.println("Un client si è collegato!");
+
+    //    do{
+    //    Socket s = server.accept();
+    //    System.out.println("Un client si è collegato!");
+//
+    //        MioThread t = new MioThread(s);
+    //        t.start();
+    //    }while();
+
+
+
+
+
+
+
+
+
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaRicevuta; 
+        do {
+            stringaRicevuta = in.readLine();
+            System.out.println("La stringa ricevuta è: " + stringaRicevuta);
+    
+            String stringaMaiuscola = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaMaiuscola + "\n");
+        } while (!stringaRicevuta.equals("!"));
+        
+        s.close();
+        server.close();
     }
 }
